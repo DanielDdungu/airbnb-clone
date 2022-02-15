d0ef3736fb29a0e7e6e3a892ea04d0536e6c777b
Daniel Ddungu <daddungu@outlook.com>
2022-02-15 21:03:52 +0100

    Airbnb clone application

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FaAirbnb } from 'react-icons/fa';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar hi">
+        <FaAirbnb />
+    </nav>
+  )
+}
